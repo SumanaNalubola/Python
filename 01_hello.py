77101103c779d4fb5c862e00bb6810bb08b4fd5b
SumanaNalubola <72498879+SumanaNalubola@users.noreply.github.com>
2021-08-21 15:45:21 +0530

    Add files via upload

---
+++ 01_hello.py
@@ -0,0 +1,12 @@
+
+# single line comment
+print("hello world")
+
+''' python .\hello.py '''
+
+# multiple line print statement
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''')
+
